6c81cf3511b01d8172a59059b3746448f42738df
amanfangeria980 <amanfangeria980@gmail.com>
2024-05-17 17:38:11 +0530

    url changed

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const IMG_CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const FETCH_MENU_URL="https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D19.0759837%26lng%3D72.8776559%26restaurantId%3D";
+export const FETCH_MENU_URL="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.0759837&lng=72.8776559&restaurantId=";
